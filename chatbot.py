e36981274f05ebfe163a6156c21543caf5e22aeb
kston <kenestonsc@gmail.com>
2022-01-09 17:18:23 -0300

    newlines

--- chatbot.py
+++ chatbot.py
@@ -1,2 +1,26 @@
+import numpy as np
+import tensorflow as tf
+import re
+import time 
 
-print("oi")
+
+
+## DATA PREPROCESSING ##
+
+#Importing the dataset
+
+lines = open('./data/movie_lines.txt', encoding='utf-8', errors= 'ignore').read().split('\n')
+
+conversations = open('./data/movie_conversations.txt', encoding='utf-8', errors= 'ignore').read().split('\n')
+
+
+# creating  a dict that maps each line and its id
+
+
+id2line = {}
+
+for line in lines:
+    _line = line.split('+++$+++')
+    if len(_line) == 5:
+        id2line[_line[0]] = _line[4]       
+        
